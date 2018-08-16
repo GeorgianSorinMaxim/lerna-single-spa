5339f44680047e2f6c944c64f075c75008bc8d6d
Tarun Kumar Sukhu <tksukhu@gmail.com>
2018-08-16 16:47:12 +0530

    update single spa versions

--- packages/appOneReact/webpack.config.js
+++ packages/appOneReact/webpack.config.js
@@ -1,52 +1,52 @@
 const path = require('path');
 
 module.exports = {
-    entry: {
-        singleSpaEntry: './src/singleSpaEntry.js',
-        store: './src/store.js'
-    },
+  entry: {
+    singleSpaEntry: './src/singleSpaEntry.js',
+    store: './src/store.js'
+  },
 
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'release'),
-        libraryTarget: 'amd',
-        library: 'reactApp'
-    },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'release'),
+    libraryTarget: 'umd',
+    library: 'app1'
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.js/,
-                use: ['babel-loader?cacheDirectory'],
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.(png|jpg|gif)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            publicPath: '/app1/',
-                        }
-                    }
-                ]
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        use: ['babel-loader'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              publicPath: '/app1/'
             }
-        ],
-    },
+          }
+        ]
+      }
+    ]
+  },
 
-    mode: 'development',
+  mode: 'development',
 
-    devtool: 'eval-source-map',
-    // devtool: 'none',
+  devtool: 'eval-source-map',
+  // devtool: 'none',
 
-    devServer: {
-        historyApiFallback: true,
-        watchOptions: { aggregateTimeout: 300, poll: 1000 },
-        headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-            "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
-        }
-    },
-
+  devServer: {
+    historyApiFallback: true,
+    watchOptions: { aggregateTimeout: 300, poll: 1000 },
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers':
+        'X-Requested-With, content-type, Authorization'
+    }
+  }
 };
